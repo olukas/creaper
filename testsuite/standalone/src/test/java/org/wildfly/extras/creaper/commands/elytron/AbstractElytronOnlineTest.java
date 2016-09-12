0670524de8cf8b7da1b8b9851f3215d3f9495a21
Josef Cacek <josef.cacek@gmail.com>
2016-09-12 15:13:50 +0200

    Workaround for JBEAP-5955; Newly the handling of creaper client lifecycle is on Test level (instead of Class level)

--- testsuite/standalone/src/test/java/org/wildfly/extras/creaper/commands/elytron/AbstractElytronOnlineTest.java
+++ testsuite/standalone/src/test/java/org/wildfly/extras/creaper/commands/elytron/AbstractElytronOnlineTest.java
@@ -3,7 +3,11 @@ package org.wildfly.extras.creaper.commands.elytron;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.wildfly.extras.creaper.commands.elytron.realm.AddExtensionAndSubsystem;
 import org.wildfly.extras.creaper.commands.elytron.realm.RemoveExtensionAndSubsystem;
@@ -16,38 +20,54 @@ import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
 public abstract class AbstractElytronOnlineTest {
 
-    protected static OnlineManagementClient client;
-    protected static Operations ops;
-    protected static Administration administration;
+    protected OnlineManagementClient client;
+    protected Operations ops;
+    protected Administration administration;
     private static boolean removeSubsystemAtTheEnd = false;
 
     protected static final Address SUBSYSTEM_ADDRESS = Address.subsystem("elytron");
 
     @BeforeClass
     public static void addSubsystem() throws Exception {
-        client = ManagementClient.online(OnlineOptions.standalone().localDefault().build());
-        ops = new Operations(client);
-        administration = new Administration(client);
-        if (!ops.exists(SUBSYSTEM_ADDRESS)) {
-            AddExtensionAndSubsystem addExtensionAndSubsystem = new AddExtensionAndSubsystem();
-            client.apply(addExtensionAndSubsystem);
-            removeSubsystemAtTheEnd = true;
+        try (OnlineManagementClient client = createManagementClient()) {
+            Operations ops = new Operations(client);
+            if (!ops.exists(SUBSYSTEM_ADDRESS)) {
+                AddExtensionAndSubsystem addExtensionAndSubsystem = new AddExtensionAndSubsystem();
+                client.apply(addExtensionAndSubsystem);
+                removeSubsystemAtTheEnd = true;
+            }
+            assertTrue("The Elytron subsystem must exist.", ops.exists(SUBSYSTEM_ADDRESS));
         }
-        assertTrue("The Elytron subsystem must exist.", ops.exists(SUBSYSTEM_ADDRESS));
     }
 
     @AfterClass
     public static void removeSubsystem() throws Exception {
-        try {
+        try (OnlineManagementClient client = createManagementClient()) {
+            Operations ops = new Operations(client);
+            Administration administration = new Administration(client);
             if (removeSubsystemAtTheEnd && ops.exists(SUBSYSTEM_ADDRESS)) {
                 RemoveExtensionAndSubsystem removeExtensionAndSubsystem = new RemoveExtensionAndSubsystem();
                 client.apply(removeExtensionAndSubsystem);
-                administration.reloadIfRequired();
-                assertFalse("The Elytron was no removed.", ops.exists(SUBSYSTEM_ADDRESS));
+                administration.reload();
+                assertFalse("The Elytron subsystem should not be present anymore.",
+                        ops.exists(SUBSYSTEM_ADDRESS));
             }
-        } finally {
-            removeSubsystemAtTheEnd = false;
-            client.close();
         }
+    }
+
+    @Before
+    public void setupCreaperForTest() throws IOException {
+        client = createManagementClient();
+        ops = new Operations(client);
+        administration = new Administration(client);
+    }
+
+    @After
+    public void tearDownCreaperForTest() throws IOException {
+        client.close();
+    }
+
+    protected static OnlineManagementClient createManagementClient() throws IOException {
+        return ManagementClient.online(OnlineOptions.standalone().localDefault().build());
     }
 }
