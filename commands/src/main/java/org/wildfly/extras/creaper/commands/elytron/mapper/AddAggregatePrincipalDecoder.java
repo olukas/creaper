4bc3f1c7b5d574cc0acd7190f215399a851f0996
Jan Tymel <jtymel@redhat.com>
2016-09-14 15:33:43 +0200

    Add commands for aggregate-principal-decoder and a little redesign of AddConcatenatingPrincipalDecoder

--- commands/src/main/java/org/wildfly/extras/creaper/commands/elytron/mapper/AddAggregatePrincipalDecoder.java
+++ commands/src/main/java/org/wildfly/extras/creaper/commands/elytron/mapper/AddAggregatePrincipalDecoder.java
@@ -6,53 +6,40 @@ import org.wildfly.extras.creaper.core.online.operations.Operations;
 import org.wildfly.extras.creaper.core.online.operations.Values;
 import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
-public final class AddConcatenatingPrincipalDecoder extends AbstractAddPrincipalDecoder {
+public final class AddAggregatePrincipalDecoder extends AbstractAddPrincipalDecoder {
 
-    private final String joiner;
-
-    public AddConcatenatingPrincipalDecoder(Builder builder) {
+    public AddAggregatePrincipalDecoder(Builder builder) {
         super(builder);
-        this.joiner = builder.joiner;
     }
 
     @Override
     public void apply(OnlineCommandContext ctx) throws Exception {
         Operations ops = new Operations(ctx.client);
         Address constantPrincipalDecoderAddress = Address.subsystem("elytron")
-                .and("concatenating-principal-decoder", name);
+                .and("aggregate-principal-decoder", name);
         if (replaceExisting) {
             ops.removeIfExists(constantPrincipalDecoderAddress);
             new Administration(ctx.client).reloadIfRequired();
         }
 
         ops.add(constantPrincipalDecoderAddress, Values.empty()
-                .andList(String.class, "principal-decoders", principalDecoders)
-                .andOptional("joiner", joiner));
+                .andList(String.class, "principal-decoders", principalDecoders));
     }
 
     public static final class Builder extends AbstractAddPrincipalDecoder.Builder<Builder> {
-
-        private String joiner;
 
         public Builder(String name) {
             super(name);
         }
 
-        public Builder joiner(String joiner) {
-            if (joiner == null || joiner.isEmpty()) {
-                throw new IllegalArgumentException("Joiner must not be null and must have a minimum length of 1 character");
-            }
-            this.joiner = joiner;
-            return this;
-        }
-
         @Override
-        public AddConcatenatingPrincipalDecoder build() {
+        public AddAggregatePrincipalDecoder build() {
             if (principalDecoders.size() < 2) {
                 throw new IllegalArgumentException("There must be at least two principal-decoders");
             }
-            return new AddConcatenatingPrincipalDecoder(this);
+            return new AddAggregatePrincipalDecoder(this);
         }
+
     }
 
 }
--- testsuite/standalone/src/test/java/org/wildfly/extras/creaper/commands/elytron/mapper/AddConcatenatingPrincipalDecoderOnlineTest.java
+++ testsuite/standalone/src/test/java/org/wildfly/extras/creaper/commands/elytron/mapper/AddConcatenatingPrincipalDecoderOnlineTest.java
@@ -1,14 +1,11 @@
 package org.wildfly.extras.creaper.commands.elytron.mapper;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.extras.creaper.commands.elytron.AbstractElytronOnlineTest;
 import org.wildfly.extras.creaper.core.CommandFailedException;
 import org.wildfly.extras.creaper.core.online.ModelNodeResult;
 import org.wildfly.extras.creaper.core.online.operations.Address;
@@ -18,7 +15,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 @RunWith(Arquillian.class)
-public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractElytronOnlineTest {
+public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractAddPrincipalDecoderOnlineTest {
 
     private static final String TEST_CONCATENATING_PRINCIPAL_DECODER_NAME = "CreaperTestConcatenatingPrincipalDecoder";
     private static final Address TEST_CONCATENATING_PRINCIPAL_DECODER_ADDRESS = SUBSYSTEM_ADDRESS
@@ -27,36 +24,10 @@ public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractElytronO
     private static final Address TEST_CONCATENATING_PRINCIPAL_DECODER_ADDRESS2 = SUBSYSTEM_ADDRESS
             .and("concatenating-principal-decoder", TEST_CONCATENATING_PRINCIPAL_DECODER_NAME2);
 
-    private static final String TEST_CONSTANT_PRINCIPAL_DECODER_NAME = "CreaperTestConstantPrincipalDecoder";
-    private static final Address TEST_CONSTANT_PRINCIPAL_DECODER_ADDRESS = SUBSYSTEM_ADDRESS
-            .and("constant-principal-decoder", TEST_CONSTANT_PRINCIPAL_DECODER_NAME);
-    private static final String TEST_CONSTANT_PRINCIPAL_DECODER_NAME2 = "CreaperTestConstantPrincipalDecoder2";
-    private static final Address TEST_CONSTANT_PRINCIPAL_DECODER_ADDRESS2 = SUBSYSTEM_ADDRESS
-            .and("constant-principal-decoder", TEST_CONSTANT_PRINCIPAL_DECODER_NAME2);
-
-    private static final List<String> EXPECTED_PRINCIPAL_DECODERS
-            = Arrays.asList(TEST_CONSTANT_PRINCIPAL_DECODER_NAME, TEST_CONSTANT_PRINCIPAL_DECODER_NAME2);
-
-    @Before
-    public void addPrincipalDecoders() throws Exception {
-        AddConstantPrincipalDecoder addConstantPrincipalDecoder
-                = new AddConstantPrincipalDecoder.Builder(TEST_CONSTANT_PRINCIPAL_DECODER_NAME)
-                .constant("role1")
-                .build();
-        AddConstantPrincipalDecoder addConstantPrincipalDecoder2
-                = new AddConstantPrincipalDecoder.Builder(TEST_CONSTANT_PRINCIPAL_DECODER_NAME2)
-                .constant("role2")
-                .build();
-        client.apply(addConstantPrincipalDecoder);
-        client.apply(addConstantPrincipalDecoder2);
-    }
-
     @After
     public void cleanup() throws Exception {
         ops.removeIfExists(TEST_CONCATENATING_PRINCIPAL_DECODER_ADDRESS);
         ops.removeIfExists(TEST_CONCATENATING_PRINCIPAL_DECODER_ADDRESS2);
-        ops.removeIfExists(TEST_CONSTANT_PRINCIPAL_DECODER_ADDRESS);
-        ops.removeIfExists(TEST_CONSTANT_PRINCIPAL_DECODER_ADDRESS2);
         administration.reloadIfRequired();
     }
 
@@ -73,7 +44,7 @@ public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractElytronO
         assertTrue("Concatenating-principal-decoder should be created",
                 ops.exists(TEST_CONCATENATING_PRINCIPAL_DECODER_ADDRESS));
         checkConcatenatingPrincipalDecoderAttribute("joiner", ".");
-        checkConcatenatingPrincipalDecoderAttribute("principal-decoders", EXPECTED_PRINCIPAL_DECODERS);
+        checkConcatenatingPrincipalDecoderAttribute("principal-decoders", PRINCIPAL_DECODERS_1_AND_2);
     }
 
     @Test
@@ -127,7 +98,7 @@ public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractElytronO
         AddConcatenatingPrincipalDecoder addConcatenatingPricipalDecoder2
                 = new AddConcatenatingPrincipalDecoder.Builder(TEST_CONCATENATING_PRINCIPAL_DECODER_NAME)
                 .joiner("::")
-                .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME, TEST_CONSTANT_PRINCIPAL_DECODER_NAME2)
+                .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME2, TEST_DIFFERENT_PRINCIPAL_DECODER_NAME)
                 .replaceExisting()
                 .build();
 
@@ -139,6 +110,7 @@ public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractElytronO
         assertTrue("Concatenating-principal-decoder should be created",
                 ops.exists(TEST_CONCATENATING_PRINCIPAL_DECODER_ADDRESS));
         checkConcatenatingPrincipalDecoderAttribute("joiner", "::");
+        checkConcatenatingPrincipalDecoderAttribute("principal-decoders", PRINCIPAL_DECODERS_2_AND_DIFFERENT);
     }
 
     @Test(expected = CommandFailedException.class)
@@ -156,40 +128,40 @@ public class AddConcatenatingPrincipalDecoderOnlineTest extends AbstractElytronO
     @Test(expected = IllegalArgumentException.class)
     public void addConcatenatingPrincipalDecoder_nullName() throws Exception {
         new AddConcatenatingPrincipalDecoder.Builder(null)
-            .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME, TEST_CONSTANT_PRINCIPAL_DECODER_NAME2)
-            .build();
+                .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME, TEST_CONSTANT_PRINCIPAL_DECODER_NAME2)
+                .build();
         fail("Creating command with null name should throw exception");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void addConcatenatingPrincipalDecoder_emptyName() throws Exception {
         new AddConcatenatingPrincipalDecoder.Builder("")
-            .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME, TEST_CONSTANT_PRINCIPAL_DECODER_NAME2)
-            .build();
+                .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME, TEST_CONSTANT_PRINCIPAL_DECODER_NAME2)
+                .build();
         fail("Creating command with empty name should throw exception");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void addConcatenatingPrincipalDecoder_nullPrincipalDecoders() throws Exception {
         new AddConcatenatingPrincipalDecoder.Builder(TEST_CONCATENATING_PRINCIPAL_DECODER_NAME)
-            .principalDecoders(null)
-            .build();
+                .principalDecoders(null)
+                .build();
         fail("Creating command with null principal-decoders should throw exception");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void addConcatenatingPrincipalDecoder_emptyPrincipalDecoders() throws Exception {
         new AddConcatenatingPrincipalDecoder.Builder(TEST_CONCATENATING_PRINCIPAL_DECODER_NAME)
-            .principalDecoders("")
-            .build();
+                .principalDecoders("")
+                .build();
         fail("Creating command with empty principal-decoders should throw exception");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void addConcatenatingPrincipalDecoder_onePrincipalDecoder() throws Exception {
         new AddConcatenatingPrincipalDecoder.Builder(TEST_CONCATENATING_PRINCIPAL_DECODER_NAME)
-            .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME)
-            .build();
+                .principalDecoders(TEST_CONSTANT_PRINCIPAL_DECODER_NAME)
+                .build();
         fail("Creating command with only one principal-decoder should throw exception");
     }
 
